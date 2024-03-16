2f15bb1a8e243c34437ce1279ee4792aa095ceba
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2024-03-16 02:05:54 -0400

    public

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
